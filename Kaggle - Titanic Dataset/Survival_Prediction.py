3f86209b428e46610e92fd84b62899f135f5f5af
Smit Patel <smipatel@cisco.com>
2020-05-16 22:43:13 +0530

    Binary Classification

--- Kaggle - Titanic Dataset/Survival_Prediction.py
+++ Kaggle - Titanic Dataset/Survival_Prediction.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn import svm
+from sklearn import tree
 from sklearn import metrics
+from sklearn import ensemble
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
@@ -147,3 +150,21 @@ class FineTune():
         print("*****==========*****")
         print()
         return (search.best_params_, search.best_estimator_)
+
+impCols = ['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+trainSet = pd.read_csv("Train_Subset.csv", sep=",", usecols=impCols)
+
+labels = ['Survived']
+trainLabels = trainSet[labels].astype('category')
+trainAttrs = trainSet.drop(labels, axis=1)
+
+utils = Utils()
+preProcess = PreProcess()
+validateModels = ValidateModels(10)
+
+X_train = preProcess.perform(trainAttrs)
+Y_train = trainLabels.values.ravel()
+
+svm_model = svm.SVC(random_state=50)
+rf_model = ensemble.RandomForestClassifier(random_state=50, n_jobs=-1)
+gdb_model = ensemble.GradientBoostingClassifier(random_state=50)
